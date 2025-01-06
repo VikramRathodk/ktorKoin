ec333ee4baa06395d532e7e6a8c369793273e985
Vikram Rathod <vikram.rathod@comittechnology.com>
2025-01-06 21:41:26 +0530

    Animation

--- composeApp/src/androidMain/kotlin/org/devvikram/ktorkoin/navigation/screens/HomeScreen.kt
+++ composeApp/src/androidMain/kotlin/org/devvikram/ktorkoin/navigation/screens/HomeScreen.kt
@@ -1,10 +1,25 @@
 package org.devvikram.ktorkoin.navigation.screens
 
 import android.widget.Toast
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.DropdownMenuItem
@@ -12,18 +27,22 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ExposedDropdownMenuBox
 import androidx.compose.material.ExposedDropdownMenuDefaults
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Snackbar
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import org.devvikram.ktorkoin.data.model.States
 import org.devvikram.ktorkoin.presentation.viewmodels.StateViewmodel
 import org.devvikram.ktorkoin.presentation.viewmodels.UIState
 
@@ -87,10 +106,77 @@ fun StateSpinner(
 ) {
     var expanded by remember { mutableStateOf(false) }
     var selectedState by remember { mutableStateOf("") }
-
     Column {
-        Text(text = "Select a State", style = MaterialTheme.typography.subtitle1)
 
+
+        var isClicked by remember { mutableStateOf(false) }
+        val backgroundColor by animateColorAsState(
+            targetValue = if (isClicked) Color.Red else Color.Blue, label = ""
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(120.dp)
+                .background(backgroundColor)
+                .clickable { isClicked = !isClicked }
+        ) {
+            Text(text = "Tab to Change Color", color = Color.White)
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            var count by remember { mutableIntStateOf(0) }
+            Button(onClick = { count++ }) {
+                Text("Add")
+            }
+            AnimatedContent(
+                targetState = count,
+                label = "animated content"
+            ) { targetCount ->
+                Text(text = "Count: $targetCount")
+            }
+        }
+        var isFirst by remember { mutableStateOf(true) }
+
+        Crossfade(targetState = isFirst, label = "") { screen ->
+            if (screen) {
+                Text("First Screen")
+            } else {
+                Text("Second Screen")
+            }
+        }
+
+        Button(onClick = { isFirst = !isFirst }) {
+            Text("Switch Screen")
+        }
+
+
+        var expanded by remember { mutableStateOf(false) }
+        val size by animateDpAsState(
+            targetValue = if (expanded) 100.dp else 50.dp,
+            animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)
+        )
+
+        Box(
+            modifier = Modifier
+                .size(size)
+                .background(Color.Yellow)
+                .clickable { expanded = !expanded }
+        )
+
+
+
+
+
+
+
+
+
+        Text(text = "Select a State", style = MaterialTheme.typography.subtitle1)
         ExposedDropdownMenuBox(
             expanded = expanded,
             onExpandedChange = { expanded = !expanded }
@@ -105,7 +191,6 @@ fun StateSpinner(
                 },
                 modifier = Modifier.fillMaxWidth()
             )
-
             ExposedDropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false }
